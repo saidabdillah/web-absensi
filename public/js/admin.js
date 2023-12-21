02dd2e00d4e9f44c232f156989f5da7819fdf996
saidabdillah <msaidabdillah18@gamil.com>
2023-12-21 21:29:52 +0800

    Memperbaiki error file admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,14 +1,15 @@
-const latitude = document.querySelector(".latitude").textContent;
-const longitude = document.querySelector(".longitude").textContent;
-console.log(latitude);
-const map = L.map("map").setView([latitude, longitude], 13);
+if (document.querySelector(".latitude")) {
+    const latitude = document.querySelector(".latitude").textContent;
+    const longitude = document.querySelector(".longitude").textContent;
+    const map = L.map("map").setView([latitude, longitude], 13);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
-L.marker([latitude, longitude])
-    .addTo(map)
-    .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-    .openPopup();
+    L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+        .openPopup();
+}
